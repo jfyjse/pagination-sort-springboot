485b635c2b1da613ca11082333bf1703c8dd6997
jfyjse <41021739+jfyjse@users.noreply.github.com>
2021-11-09 12:14:11 +0530

    Update ProductService.java

--- src/main/java/com/joffy/paginationandsort/service/ProductService.java
+++ src/main/java/com/joffy/paginationandsort/service/ProductService.java
@@ -19,13 +19,14 @@ public class ProductService {
     ProductRepository productRepository;
 
     // adding dummy products
-//    @PostConstruct
-//    public void initDb(){
-//        List<ProductEntity> products = IntStream.rangeClosed(1,400)
-//                .mapToObj(i-> new ProductEntity("product n"+i,new Random().nextInt(100),
-//                        new Random().nextInt(5000))).collect(Collectors.toList());
-//        productRepository.saveAll(products);
-//    }
+    
+   @PostConstruct
+   public void initDb(){
+       List<ProductEntity> products = IntStream.rangeClosed(1,400)
+               .mapToObj(i-> new ProductEntity("product n"+i,new Random().nextInt(100),
+                       new Random().nextInt(5000))).collect(Collectors.toList());
+       productRepository.saveAll(products);
+   }
 
     public List<ProductEntity> findAllProducts(){
         return productRepository.findAll();
